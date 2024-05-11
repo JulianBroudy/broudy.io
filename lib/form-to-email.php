0d24cba1f782604b521278bba12b13e2ed2d6b5a
Julian Broudy <Julian@Broudy.io>
2024-05-11 16:33:42 +0300

    Remove exit statement from form-to-email.php

--- lib/form-to-email.php
+++ lib/form-to-email.php
@@ -3,7 +3,7 @@ if (!isset($_POST['submit-button']))
 {
     // This page should not be accessed directly. Need to submit the form.
     echo "error; you need to submit the form!";
-    exit;
+//     exit;
 }
 $visitor_email = $_POST['inputEmail'];
 $subject = $_POST['inputSubject'];
